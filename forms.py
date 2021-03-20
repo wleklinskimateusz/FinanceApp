ecd9ed01c9ce0e8093090f45ec59b1db3f0ca699
matix0508 <matix0508@gmail.com>
2021-03-20 23:56:34 +0100

    login system

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
 
 
--- routes.py
+++ routes.py
@@ -1,18 +1,33 @@
 from app import app, db, login_manager
 from flask import render_template, redirect, url_for
-from forms import CategoryForm, ExpenseForm
+from forms import CategoryForm, ExpenseForm, LoginForm
 from models import Category, User, Expense
+from flask_login import current_user, login_required, logout_user, login_user
 
 
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @login_manager.unauthorized_handler
 def unauthorized():
     return render_template('error.html', title="You don't have access", photo='/static/images/not_pass.jpg')
 
+@app.route("/nope")
+@login_required
+def nope():
+    return render_template(
+        'error.html',
+        photo="/static/images/wrong.jpg",
+        title="Well..."
+    )
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -22,6 +37,20 @@ def page_not_found(e):
                            text="Daaaamn, Looks like you have wrong Address",
                            title="No Can Do"
                            ), 404
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            return redirect(url_for('nope'))
+        login_user(user, remember=False)
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form)
 
 
 @app.route('/')
